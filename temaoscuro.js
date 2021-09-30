c484b0705ea47e6bbf2d9cc145089115c0117c55
fsarvi <91506958+fsarvi@users.noreply.github.com>
2021-09-30 14:04:44 -0300

    Update temaoscuro.js

--- temaoscuro.js
+++ temaoscuro.js
@@ -22,18 +22,18 @@ function cambiar_tema() {
 
   var footer = document.getElementById("footer");
     if (footer.style.backgroundColor === "#bd3133"){
-      footer.style.backgroundColor= "#535252";
+      footer.style.backgroundColor = "#535252";
     } else{
-      footer.style.backgroundColor= "#bd3133"
+      footer.style.backgroundColor = "#bd3133"
     }
     
 
   var header = document.getElementById("header");
     
-    if (header.style.backgroundColor=== "#bd3133"){
-      header.style.backgroundColor= "#535252";
+    if (header.style.backgroundColor === "#bd3133"){
+      header.style.backgroundColor = "#535252";
     } else{
-      header.style.backgroundColor= "#bd3133"
+      header.style.backgroundColor = "#bd3133"
     }
     
 }
@@ -44,10 +44,10 @@ document.querySelector("#tema_oscuro_menu")
 function cambiar_tema() {
 
   var footer = document.getElementById("footer");
-    if (footer.style.backgroundColor ==="#bd3133"){
-      footer.style.backgroundColor= "#535252";
+    if (footer.style.backgroundColor === "#bd3133"){
+      footer.style.backgroundColor = "#535252";
     } else if (footer.style.backgroundColor= "#535252"){
-      footer.style.backgroundColor= "#bd3133"
+      footer.style.backgroundColor = "#bd3133"
     }
     
 
