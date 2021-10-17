ae9d5296ea1a6c32336d9f7eba5fa5acbf135aab
fsarvi <91506958+fsarvi@users.noreply.github.com>
2021-10-17 20:52:10 -0300

    Update temaoscuro.js

--- temaoscuro.js
+++ temaoscuro.js
@@ -1,33 +1,23 @@
 function temaOscuro(){
-  var fondo = document.querySelector(".footer").style.backgroundColor;
-  alert(fondo);
-  if (fondo === "rgb(189, 49, 51)"){
-    console.log("negro");
+  var footer = document.querySelector(".footer").style.backgroundColor;
+
+  if (footer === "rgb(189, 49, 51)"){
+
     document.querySelector(".footer").style.backgroundColor = '#535252';
   }
   else {
-    console.log("rojo");
+ 
     document.querySelector(".footer").style.backgroundColor = '#bd3133';
   }
-}
+  var header = document.querySelector(".header").style.backgroundColor;
 
-/*document.querySelector(".temaoscuro")
-  .addEventListener("click", cambiar_tema);
+  if (fondo1 === "rgb(189, 49, 51)"){
 
-function cambiar_tema() {
-  const rojo = '#bd3133';
-  const oscuro = '#535252';
+    document.querySelector(".header").style.backgroundColor = '#535252';
+  }
+  else {
+
+    document.querySelector(".header").style.backgroundColor = '#bd3133';
+  }
   
-  var footer = document.getElementById('footer');
-  
-    if (footer.style.backgroundColor === '#bd3133'){
-      console.log("negro");
-      footer.style.backgroundColor = '#535252';
-    }
-    else {
-      console.log("rojo");
-      footer.style.backgroundColor = '#bd3133';
-    }
-
 }
-*/
