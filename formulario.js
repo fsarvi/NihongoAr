ed37ac1ed1c2bcbc0785f92df5102c5ccea40061
fsarvi <91506958+fsarvi@users.noreply.github.com>
2021-10-22 20:50:57 -0300

    Update formulario.js

--- formulario.js
+++ formulario.js
@@ -1,3 +1,6 @@
+/*El evento «DOMContentLoaded» que hemos asociado al objeto document: sirve para detectar cuando el navegador ya ha procesado todos los elementos de la página, momento en el cual ya somos capaces de acceder al formulario con seguridad.
+El evento «submit» que hemos asociado al formulario, que se dispara en el momento que el usuario envíe el formulario. Como manejador del evento submit hemos indicado el nombre de una función llamada «manejadorValidacion», cuyo código nos servirá para realizar las correspondientes validaciones.*/
+
 document.addEventListener("DOMContentLoaded", function() {
   document.getElementById("formulario").addEventListener('submit', validarFormulario); 
 });
@@ -16,7 +19,9 @@ function validarFormulario(e) {
    		alert('Complete este campo');
 		document.getElementById('nombre').focus();
 		return;
-	  }
+	 } else if{
+		 validarCampo(expresiones.usuario, e.target, 'usuario');
+	 }
 	
   	var apellido = document.getElementById('apellido').value;
   	if(apellido.length == 0) {
