cf1d9ad679cc10a53cc11a63be6d43d9a38fc0a2
fsarvi <91506958+fsarvi@users.noreply.github.com>
2021-10-22 20:37:31 -0300

    Update formulario.js

--- formulario.js
+++ formulario.js
@@ -1,20 +1,20 @@
-document.getElementById("formulario").addEventListener('submit', validarFormulario); 
-	
+document.addEventListener("DOMContentLoaded", function() {
+  document.getElementById("formulario").addEventListener('submit', validarFormulario); 
+});
+
 function validarFormulario(e) {
-	e.preventDefault();
-	
-	var nombre = document.getElementById("nombre").value;
-	var apellido= document.getElementById("apellido").value;
-	var opcion= document.getElementById("opcion").value;
-	var texto= document.getElementById("texto").value;
-
-	if(nombre.length == 0 || apellido.length==0 || opcion.length==0 || texto.length==0) {
-		
-		alert("Completa todos los campos antes de enviar");
-		 return ;
-	} else {
-		alert("Mensaje enviado correctamente");
-		this.submit();
-	}
+  e.preventDefault();
+  var nombre = document.getElementById('nombre').value;
+  if(nombre.length == 0) {
+    	alert('Complete este campo');
+	document.getElementById('nombre').focus();
+    return;
+  }
+  var apellido = document.getElementById('apellido').value;
+  if(apellido.length == 0) {
+    	alert('Complete este campo');
+	document.getElementById('apellido').focus();
+    return;
+  }
+  this.submit();
 }
-
