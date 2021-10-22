f54b90c1ebe57466c8b96781a44d329634bfbfe1
fsarvi <91506958+fsarvi@users.noreply.github.com>
2021-10-22 19:49:46 -0300

    Update formulario.js

--- formulario.js
+++ formulario.js
@@ -1,12 +1,12 @@
 document.getElementById("formulario").addEventListener('submit', validarFormulario); 
-
-var nombre = document.getElementById("#nombre").value;
-var apellido= document.getElementById("#apellido").value;
-var opcion= document.getElementById("#opcion").value;
-var texto= document.getElementById("#texto").value;
 	
 function validarFormulario(e) {
 	e.preventDefault();
+	
+	var nombre = document.getElementById("#nombre").value;
+	var apellido= document.getElementById("#apellido").value;
+	var opcion= document.getElementById("#opcion").value;
+	var texto= document.getElementById("#texto").value;
 
 	if(nombre.length == 0 || apellido.length==0 || opcion.length==0 || texto.length==0) {
 		
