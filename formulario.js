09edf2814eca11cbf1501b6ce8fd584ecf774abb
fsarvi <91506958+fsarvi@users.noreply.github.com>
2021-10-22 01:29:28 -0300

    Add files via upload

---
+++ formulario.js
@@ -0,0 +1,83 @@
+/*Coincidencias Basicas
+.       - Cualquier Caracter, excepto nueva linea
+\d      - Cualquier Digitos (0-9)
+\D      - No es un Digito (0-9)
+\w      - Caracter de Palabra (a-z, A-Z, 0-9, _)
+\W      - No es un Caracter de Palabra.
+\s      - Espacios de cualquier tipo. (espacio, tab, nueva linea)
+\S      - No es un Espacio, Tab o nueva linea.
+
+Limites
+\b      - Limite de Palabra
+\B      - No es un Limite de Palabra
+^       - Inicio de una cadena de texto
+$       - Final de una cadena de texto
+
+Cuantificadores:
+*       - 0 o Más
++       - 1 o Más
+?       - 0 o Uno
+{3}     - Numero Exacto
+{3,4}   - Rango de Numeros (Minimo, Maximo)
+
+Conjuntos de Caracteres
+[]      - Caracteres dentro de los brackets
+[^ ]    - Caracteres que NO ESTAN dentro de los brackets
+
+Grupos
+( )     - Grupo
+|       - Uno u otro*/
+
+const formulario = document.getElementsByClassName('formulario');
+const entradas = document.querySelectorAll('.formulario input'); /*arreglo de todos los inputs*/ */
+
+const expresiones = {
+	Nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
+	Apellido: /^[a-zA-ZÀ-ÿ\s]{1,40}$/,
+	Correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+}
+
+
+const validarFormulario = () => {
+
+	switch(e.target.name) {
+
+		case "Nombre":
+			if (expresiones.Nombre.test(e.target.value)){
+
+			} else {
+				alert("No es nombre válido")
+			}
+		break;
+
+		case "Apellido":
+			if (expresiones.Apellido.test(e.target.value)){
+
+			} else {
+				alert("No es apellido válido")
+			}
+		break;
+
+		case "texto":
+		
+		break;
+
+		case "Correo":
+			if (expresiones.Correo.test(e.target.value)){
+
+			} else {
+				alert("No es correo válido")
+			}
+		break;
+	}
+}
+
+inputs.forEach ( (input) => {
+    input.addEventListener('keyup', validarFormulario);
+    input.addEventListener('blur', validarFormulario);
+})
+
+
+formulario.addEventListener('submit', (e) => {
+    e.preventDefault();
+})
